4bb538be29d27f19273fd79a15cbd270460417a8
Amit Joshi <35733459+amitjoshi9627@users.noreply.github.com>
2019-04-21 13:22:44 +0530

    Resolved issues with song playback time

--- song_processing.py
+++ song_processing.py
@@ -7,6 +7,19 @@ import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
+def song_interrupt_check(browser,song_time):
+	timer  = browser.find_element_by_xpath('//*[@id="track-elapsed"]').text
+	track_time = to_seconds(timer)
+
+	if track_time < song_time:
+		time.sleep(song_time - track_time)
+	while track_time < song_time :
+		timer  = browser.find_element_by_xpath('//*[@id="track-elapsed"]').text
+		track_time = to_seconds(timer)
+
+		if track_time < song_time:
+			time.sleep(song_time - track_time)
+
 def more_than_1_song_play(browser,num_songs = 2):
 	song_number = 2
 	while(song_number<=num_songs):
@@ -17,28 +30,27 @@ def more_than_1_song_play(browser,num_songs = 2):
 			album_name = browser.find_element_by_xpath('/html/body/article/div/div[3]/section/div[3]/div/span/a').text
 			timer = browser.find_element_by_xpath('//*[@id="track-time"]').text
 			song_time = to_seconds(timer)	
-			# print(f'{songs}.Playing Song {track_name} from the album {album_name}...\n')
+			print(f'{song_number}.Playing Song "{track_name}" from the album "{album_name}"...\n')
 			show_notificaton(track_name, album_name)
 			time.sleep(song_time-2)
 
-			timer  = browser.find_element_by_xpath('//*[@id="track-elapsed"]').text
-			track_elapsed = to_seconds(timer)
-
-			if track_elapsed < song_time:
-				time.sleep(song_time - track_elapsed -1) 
+			song_interrupt_check(browser,song_time)
 			song_number+=1
 		except:
-			close_popup_ad(browser)
+			try:
+				close_popup_ad(browser)
+			except:
+				close_login_box(browser)
 
 def play_user_timed_songs(browser,song_time = 20,num_songs = 25):
 	browser.find_elements_by_xpath('/html/body/div[8]/div/section/div/div[2]/div[2]/div/div/button')[0].click()
 	time.sleep(1)
 	track_name = browser.find_element_by_xpath('/html/body/article/div/div[3]/section/div[3]/div/strong/a').text
 	album_name = browser.find_element_by_xpath('/html/body/article/div/div[3]/section/div[3]/div/span/a').text
-	# print(f'\n{1}.Playing Song {track_name} from the album {album_name}...\n')
+	print(f'\n{1}.Playing Song "{track_name}" from the album "{album_name}"...\n')
 	show_notificaton(track_name, album_name)
 	time.sleep(song_time)
-
+	song_interrupt_check(browser,song_time)
 	song_number = 2
 	while(song_number<=num_songs):
 		try:
@@ -46,17 +58,17 @@ def play_user_timed_songs(browser,song_time = 20,num_songs = 25):
 			time.sleep(1)
 			track_name = browser.find_element_by_xpath('/html/body/article/div/div[3]/section/div[3]/div/strong/a').text
 			album_name = browser.find_element_by_xpath('/html/body/article/div/div[3]/section/div[3]/div/span/a').text
-			# print(f'{songs}.Playing Song {track_name} from the album {album_name}...\n')
+			print(f'{song_number}.Playing Song "{track_name}" from the album "{album_name}"...\n')
 			show_notificaton(track_name, album_name)
 			time.sleep(song_time)
-			timer  = browser.find_element_by_xpath('//*[@id="track-elapsed"]').text
-			track_time = to_seconds(timer)
-
-			if track_time < song_time:
-				time.sleep(song_time - track_time) 
+			song_interrupt_check(browser,song_time)
 			song_number+=1
 		except:
-			close_popup_ad(browser)
+			try:
+				close_popup_ad(browser)
+			except:
+				close_login_box(browser)
+
 	browser.find_element_by_xpath('//*[@id="pause"]').click()
 
 def play_default_timed_songs(browser,num_songs):
@@ -68,13 +80,17 @@ def play_default_timed_songs(browser,num_songs):
 		track_name = browser.find_element_by_xpath('/html/body/article/div/div[3]/section/div[3]/div/strong/a').text
 		album_name = browser.find_element_by_xpath('/html/body/article/div/div[3]/section/div[3]/div/span/a').text
 		show_notificaton(track_name, album_name)
-		# print(f'\n{1}.Playing Song {track_name} from the album {album_name}...\n')
+		print(f'\n{1}.Playing Song "{track_name}" from the album "{album_name}"...\n')
 		time.sleep(song_time-2)
+		song_interrupt_check(browser,song_time)
 		if num_songs >1:
 			more_than_1_song_play(browser,num_songs)
 
 	except:
-		close_popup_ad(browser)
+		try:
+			close_popup_ad(browser)
+		except:
+			close_login_box(browser)
 		if num_songs >1:
 			more_than_1_song_play(browser,num_songs)
 	browser.find_element_by_xpath('//*[@id="pause"]').click()
